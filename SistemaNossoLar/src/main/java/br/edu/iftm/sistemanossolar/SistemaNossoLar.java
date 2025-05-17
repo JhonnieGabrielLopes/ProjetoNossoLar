e37cef557f384b642844a1697cde7a6dd8df74ee
jhonnie <jhonniegabriellg@gmail.com>
2025-05-17 10:32:24 -0300

    conexao com o banco de dados

--- SistemaNossoLar/src/main/java/br/edu/iftm/sistemanossolar/SistemaNossoLar.java
+++ SistemaNossoLar/src/main/java/br/edu/iftm/sistemanossolar/SistemaNossoLar.java
@@ -6,7 +6,7 @@ import br.edu.iftm.sistemanossolar.dao.ConexaoBanco;
 
 public class SistemaNossoLar {
 
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args) {
         System.out.println("Hello World!");
         Connection conn = ConexaoBanco.getConnection();
     }
